903431dfadf1573f3ea7b0af105915198f370a0a
e.kozlov <e.kozlov@gaijin.team>
2025-01-30 09:35:27 +0300

    support timings and compile on windows

--- build.vc.cmd
+++ build.vc.cmd
@@ -7,7 +7,7 @@ if not defined VSINSTALLDIR (
 )
 
 pushd jam_src
-cl -nologo -W0 -O2 -DNT -DWIN32 builtins.c command.c compile.c expand.c execdmc.c filent.c glob.c hash.c headers.c jam.c jambase.c jamgram.c lists.c make.c make1.c newstr.c option.c parse.c pathunix.c  regexp.c rules.c scan.c search.c variable.c timestamp.c outFilter.c user32.lib /Fe../jam_0.exe
+cl -nologo -W0 -O2 -DNT -DWIN32 builtins.c command.c compile.c expand.c execdmc.c filent.c glob.c hash.c headers.c jam.c jambase.c jamgram.c lists.c make.c make1.c newstr.c option.c parse.c pathunix.c  regexp.c rules.c scan.c search.c variable.c timestamp.c outFilter.c buildstats.c user32.lib /Fe../jam_0.exe
 del *.obj
 popd
 jam_0 -sRoot=. -f jam_src/Jamfile clean_all
--- jam_src/execdmc.c
+++ jam_src/execdmc.c
@@ -653,6 +653,45 @@ void __cdecl onintr( int disp )
     printf("...interrupted - be patient while processes finish working\n");
 }
 
+#if !defined(unix)
+
+#define CLOCK_MONOTONIC 0
+#define MS_PER_SEC      1000ULL     // MS = milliseconds
+#define US_PER_MS       1000ULL     // US = microseconds
+#define HNS_PER_US      10ULL       // HNS = hundred-nanoseconds (e.g., 1 hns = 100 ns)
+#define NS_PER_US       1000ULL
+
+#define HNS_PER_SEC     (MS_PER_SEC * US_PER_MS * HNS_PER_US)
+#define NS_PER_HNS      (100ULL)    // NS = nanoseconds
+#define NS_PER_SEC      (MS_PER_SEC * US_PER_MS * NS_PER_US)
+
+int clock_gettime_monotonic(struct timespec *tv)
+{
+    static LARGE_INTEGER ticksPerSec;
+    LARGE_INTEGER ticks;
+
+    if (!ticksPerSec.QuadPart) {
+        QueryPerformanceFrequency(&ticksPerSec);
+        if (!ticksPerSec.QuadPart) {
+            errno = ENOTSUP;
+            return -1;
+        }
+    }
+
+    QueryPerformanceCounter(&ticks);
+
+    tv->tv_sec = (long)(ticks.QuadPart / ticksPerSec.QuadPart);
+    tv->tv_nsec = (long)(((ticks.QuadPart % ticksPerSec.QuadPart) * NS_PER_SEC) / ticksPerSec.QuadPart);
+
+    return 0;
+}
+
+int clock_gettime(int v, struct timespec *tp)
+{
+    return clock_gettime_monotonic(tp);
+}
+#endif
+
 #if defined(unix)
 static void *__cdecl exec_job_thread(void *p)
 #else
@@ -700,7 +739,7 @@ static void __cdecl exec_job_thread(void *p)
     {
       sprintf(etaStr, "%0.2fs", (mdsec + 0.0f) / mssec);
     }
-    printf("[%5u/%u ETA %s (%i jobs)] ", ctx->stats.index, bstat_get_total_updating(), etaStr, parallelism);
+    printf("[%5u/%u ETA %s (%i jobs)]\n", ctx->stats.index, bstat_get_total_updating(), etaStr, parallelism);
   }
 
 
